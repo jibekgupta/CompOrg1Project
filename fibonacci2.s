93595cfb02028a40498da14c9d38a2cfa4a3b310
jibu059 <jibek.gupta@bison.howard.edu>
2023-11-28 18:02:35 -0500

    Loop until the input integer is greater than 24.

--- fibonacci2.s
+++ fibonacci2.s
@@ -4,3 +4,19 @@ error_msg: .asciiz "Illegal Number!\n"          #Message indicating an illegal n
 
 .text
 .globl main         #The entry point of the program
+
+li$v0,4             #Load immediate value 4 into $v0
+la$a0,prompt        #Load address of the 'prompt' message into $a0
+syscall             #Invoke syscall to print the prompt message
+
+li$v0,5             #Load immediate value 5 into $v0
+syscall             #Invoke syscall to read an integer from input
+move $t0,$v0        #Move the input value from $v0 to $t0
+
+loop1:
+bgt $t0,24,find_fibonacci       #Branch if the value in $t0 is greater than 24 to the 'find_fibonacci' label
+
+li$v0,4                         #Load immediate value 4 into $v0
+la$a0,prompt                    #Load address of the 'prompt' message into $a0
+syscall                         #Invoke syscall to print the prompt message
+
