d77aad0effcc688c3c19c5ff015dcf86fd7b9125
jibu059 <jibek.gupta@bison.howard.edu>
2023-11-28 16:36:55 -0500

    Print the 'error_msg' string using syscall 4, then unconditionally jump to the 'get_input' label for program continuation.

--- fibonacci.s
+++ fibonacci.s
@@ -16,9 +16,16 @@ li$v0,5                 #load immediate value 5 into $v0
 syscall                 #Invoke system call to read an integer from input
 move $t0,$v0            #move the input value from $v0 to $t0
 
-blez $t0,Illegal        #Branch to 'Illegal' label if the value in $t0 is less than or equal to zero
+blez $t0,illegal        #Branch to 'Illegal' label if the value in $t0 is less than or equal to zero
 li$t1,25                #Load immediate value 25 into register $t1
-blt $t0,$t1,Illegal     #Branch to 'Illegal' label if the value in $t0 is less than the value in $t1
+blt $t0,$t1,illegal     #Branch to 'Illegal' label if the value in $t0 is less than the value in $t1
 
 li$v0,10                #Load immediate value 10 into register $v0
 syscall                 #Invoke system call to terminate the program
+
+illegal:
+li$v0,4                 #Load immediate value 4 into $v0
+la$a0, error_msg        #Load address of the 'error_msg' string into $a0
+syscall                 #Invoke syscall to print the error message
+
+jget_input              #Jump (unconditional) to the 'get_input' label
