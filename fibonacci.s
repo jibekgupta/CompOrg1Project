84a96a34b1e44f7e9429fd0dc8cdc57ba934fe56
jibu059 <jibek.gupta@bison.howard.edu>
2023-11-28 16:23:31 -0500

    Read an integer from input (syscall 5) and store it in  for further use.

--- fibonacci.s
+++ fibonacci.s
@@ -10,3 +10,8 @@ main:
 li$v0,4
 la $a0, prompt
 syscall
+
+get_input:
+li$v0,5        #load immediate value 5 into $v0
+syscall        #Invoke system call to read an integer from input
+move $t0,$v0   #move the input value from $v0 to $t0
